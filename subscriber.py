c943311b98adfefb714fd2a4d5d4f0003851aa0a
PedroLimaAlves <lima23456@discente.ufg.br>
2025-05-12 15:19:51 -0300

    Update subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -10,8 +10,8 @@ s.connect(p)  # Conecta-se ao servidor
 # Inscreve-se para os tópicos TIME, TEMP e HUMIDITY
 s.setsockopt_string(zmq.SUBSCRIBE, "TIME")      # Assina o tópico TIME
 s.setsockopt_string(zmq.SUBSCRIBE, "TEMP")      # Assina o tópico TEMP
-s.setsockopt_string(zmq.SUBSCRIBE, "HUMIDITY")  # Assina o tópico HUMIDITY
 s.setsockopt_string(zmq.SUBSCRIBE, "NOME")  # Assina o tópico NOME
+s.setsockopt_string(zmq.SUBSCRIBE, "MATRICULA")  # Assina o tópico MATRICULA
 
 for i in range(5):  # Realiza 5 iterações
     msg = s.recv()  # Recebe uma mensagem
